a47b8088a49809a672d2d7062f828e1c0e31d3fa
juparave <kandarv@gmail.com>
2021-11-04 20:07:47 -0600

    1 file modified

--- src/app/directives/droppable.directive.ts
+++ src/app/directives/droppable.directive.ts
@@ -1,11 +1,17 @@
-import {Directive, ElementRef, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import * as interact from 'interactjs';
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import interact from 'interactjs';
 
 @Directive({
-  selector: '[appDroppable]'
+  selector: '[appDroppable]',
 })
 export class DroppableDirective implements OnInit {
-
   @Input()
   options: any;
 
@@ -17,24 +23,22 @@ export class DroppableDirective implements OnInit {
   ngOnInit(): void {
     interact(this.elementRef.nativeElement)
       .dropzone(Object.assign({}, this.options || {}))
-      .on('dropactivate', event => event.target.classList.add('can-drop'))
-      .on('dragenter', event => {
-
+      .on('dropactivate', (event) => event.target.classList.add('can-drop'))
+      .on('dragenter', (event) => {
         const draggableElement = event.relatedTarget;
         const dropzoneElement = event.target;
 
         dropzoneElement.classList.add('can-catch');
         draggableElement.classList.add('drop-me');
-
       })
-      .on('dragleave', event => {
+      .on('dragleave', (event) => {
         event.target.classList.remove('can-catch', 'caught-it');
         event.relatedTarget.classList.remove('drop-me');
       })
-      .on('drop', event => {
+      .on('drop', (event) => {
         const model = (window as any).dragData;
 
-        if (typeof (model) === 'object') {
+        if (typeof model === 'object') {
           this.dropping.emit(model);
         }
         event.target.classList.add('caught-it');
@@ -45,7 +49,7 @@ export class DroppableDirective implements OnInit {
           window.getSelection().removeAllRanges();
         }
       })
-      .on('dropdeactivate', event => {
+      .on('dropdeactivate', (event) => {
         event.target.classList.remove('can-drop');
         event.target.classList.remove('can-catch');
       });
