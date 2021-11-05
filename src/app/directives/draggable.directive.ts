fcd2932e61f96a7e4533e086bbcb9790f2d875f5
juparave <kandarv@gmail.com>
2021-11-05 13:16:40 -0600

    1 file modified

--- src/app/directives/draggable.directive.ts
+++ src/app/directives/draggable.directive.ts
@@ -60,4 +60,25 @@ export class DraggableDirective implements OnInit {
         });
       });
   }
+
+  // added from https://alligator.io/js/drag-and-drop-interactjs/
+  onMove(target) {
+    const event = target.event;
+
+    const dataX = target.getAttribute('data-x');
+    const dataY = target.getAttribute('data-y');
+    const initialX = parseFloat(dataX) || 0;
+    const initialY = parseFloat(dataY) || 0;
+
+    const deltaX = event.dx;
+    const deltaY = event.dy;
+
+    const newX = initialX + deltaX;
+    const newY = initialY + deltaY;
+
+    target.style.transform = `translate(${newX}px, ${newY}px)`;
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
 }
