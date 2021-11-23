13ce063a05df2eeae17a04ba5a511a59839a1003
juparave <kandarv@gmail.com>
2021-11-23 12:46:19 -0600

    3 files modified

--- src/app/pages/fabricjs/fabricjs.component.ts
+++ src/app/pages/fabricjs/fabricjs.component.ts
@@ -4,45 +4,52 @@ import { fabric } from 'fabric-with-gestures';
 @Component({
   selector: 'app-fabricjs',
   templateUrl: './fabricjs.component.html',
-  styleUrls: ['./fabricjs.component.css']
+  styleUrls: ['./fabricjs.component.css'],
 })
 export class FabricjsComponent implements OnInit {
   canvas: any;
   selectedFile: File;
   imgFile: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.canvas = new fabric.Canvas('canvas', {
       selection: false,
       controlsAboveOverlay: true,
       centeredScaling: true,
-      allowTouchScrolling: true
+      allowTouchScrolling: true,
     });
 
     // hiding transform controls
-    fabric.Object.prototype._controlsVisibility = { ml: false, mb: false, mr: false, mt: false };
+    fabric.Object.prototype._controlsVisibility = {
+      ml: false,
+      mb: false,
+      mr: false,
+      mt: false,
+    };
 
     this.method2();
-
   }
 
   method2() {
     //  Method 2 (add by picture path)
-    fabric.Image.fromURL('assets/TL-2930_1.png', (img) => {
-      img.set({
-        left: 100, //  Image of the picture relative to the left side of the canvas
-        top: 100, //  Picture of the top distance of the canvas
-        angle: 30, //  Picture rotation angle
-        opacity: 1.00, //  Picture transparency
-        //  Here you can set the size of the picture after SCALEX and SCALEY, here is half the size of the original size
-        scaleX: 0.5,
-        scaleY: 0.5
-      });
-      //  Add object
-      this.canvas.add(img);
-    });
+    fabric.Image.fromURL(
+      'https://github.com/juparave/angular-image-overlay/raw/master/src/assets/TL-5150-CAN.png',
+      (img) => {
+        img.set({
+          left: 100, //  Image of the picture relative to the left side of the canvas
+          top: 100, //  Picture of the top distance of the canvas
+          angle: 30, //  Picture rotation angle
+          opacity: 1.0, //  Picture transparency
+          //  Here you can set the size of the picture after SCALEX and SCALEY, here is half the size of the original size
+          scaleX: 0.5,
+          scaleY: 0.5,
+        });
+        //  Add object
+        this.canvas.add(img);
+      }
+    );
 
     // remove some controls
     this.canvas.item(0)['ml'] = false;
@@ -51,28 +58,31 @@ export class FabricjsComponent implements OnInit {
 
   addOther() {
     //  Method 2 (add by picture path)
-    fabric.Image.fromURL('assets/TL-5150-CAN.png', (img) => {
-      img.set({
-        left: 100, //  Image of the picture relative to the left side of the canvas
-        top: 100, //  Picture of the top distance of the canvas
-        // angle: 30, //  Picture rotation angle
-        opacity: 1.00, //  Picture transparency
-        //  Here you can set the size of the picture after SCALEX and SCALEY, here is half the size of the original size
-        scaleX: 0.5,
-        scaleY: 0.5
-      });
-      //  Add object
-      let o = this.canvas.add(img);
-      console.log("Object o", o);
+    fabric.Image.fromURL(
+      'https://github.com/juparave/angular-image-overlay/raw/master/src/assets/TR-2404.B.png',
+      (img) => {
+        img.set({
+          left: 100, //  Image of the picture relative to the left side of the canvas
+          top: 100, //  Picture of the top distance of the canvas
+          // angle: 30, //  Picture rotation angle
+          opacity: 1.0, //  Picture transparency
+          //  Here you can set the size of the picture after SCALEX and SCALEY, here is half the size of the original size
+          scaleX: 0.5,
+          scaleY: 0.5,
+        });
+        //  Add object
+        let o = this.canvas.add(img);
+        console.log('Object o', o);
 
-      o['ml'] = false;
-    });
+        o['ml'] = false;
+      }
+    );
   }
 
   method1() {
     /*
- * How to add an image object to the canvas?
-*/
+     * How to add an image object to the canvas?
+     */
     //  Method 1 (added via IMG element)
     const imgElement = document.getElementById('img') as HTMLImageElement;
     const imgInstance = new fabric.Image(imgElement, {
@@ -82,7 +92,7 @@ export class FabricjsComponent implements OnInit {
       opacity: 0.85, //  Picture transparency
       //  Here you can set the size of the picture after SCALEX and SCALEY, here is half the size of the original size
       scaleX: 0.5,
-      scaleY: 0.5
+      scaleY: 0.5,
     });
     //  After adding an object, as shown below
     this.canvas.add(imgInstance);
@@ -106,9 +116,11 @@ export class FabricjsComponent implements OnInit {
         scaleY: this.canvas.height / img.height,
       });
       //  Set background
-      this.canvas.setBackgroundImage(img, this.canvas.renderAll.bind(this.canvas));
+      this.canvas.setBackgroundImage(
+        img,
+        this.canvas.renderAll.bind(this.canvas)
+      );
       this.canvas.renderAll();
     });
   }
-
 }
